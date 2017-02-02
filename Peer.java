03b2fb4bf87546bd32d3feb7ff989d16eba4eb41
Erik <eld66@nau.edu>
2017-02-02 10:42:24 -0700

    SendThread and ReceiveThread classes created

--- Peer.java
+++ Peer.java
@@ -1,5 +1,40 @@
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.lang.String;
+import java.lang.Thread;
+
 public class Peer{
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		String hostIP = args[0];
+		int portNumber = Integer.parseInt(args[1]);
+		try{
+			InetAddress hostAddress = InetAddress.getByName(hostIP);
+			new SendThread().start();
+			new ReceiveThread(hostAddress, portNumber).start();
+		}catch(UnknownHostException e){
+			System.out.println("Error: Unknown Host");
+			System.exit(1);
+		}
+	}
+	
+	static class SendThread extends Thread{
+		public void run(){
+			System.out.println("This is the thread that sends stuff");
+		}
+	}
+	
+	static class ReceiveThread extends Thread{
+		private InetAddress hostAddress;
+		private int portNumber;
+		ReceiveThread(InetAddress newHostAddress, int newPortNumber){
+			hostAddress = newHostAddress;
+			portNumber = newPortNumber;
+		}
+		public void run(){
+			System.out.println("This is the thread that receives stuff");
+		}
 	}
 }
