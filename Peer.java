93bde55c8708f19b730413a830ab11788d0fa174
Erik <eld66@nau.edu>
2017-02-12 16:01:32 -0700

    Added basic communication between peers
    No peers added to linked list yet

--- Peer.java
+++ Peer.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 public class Peer{
 	public static LinkedList<PeerInfo> peerList = new LinkedList<PeerInfo>();
     public static boolean stillConnected = true;
+	public static Scanner userInput = new Scanner(System.in);
 
 	public static void main(String[] args){
 		//IP and port number received when running program
@@ -23,13 +24,18 @@ public class Peer{
             System.exit(1);
         }
 		String hostIP = args[0];	//Take user IP from command line
-		int portNumber = Integer.parseInt(args[1]);//Take port number from command line
 		try{
 			//HostIP converted into address format
+			System.out.print("Host IP you would like to send messages to: ");
+			InetAddress receiverAddress = InetAddress.getByName(userInput.nextLine());
+			System.out.print("Port number to send to: ");
+			int receiverPort = userInput.nextInt();
+			
 			InetAddress hostAddress = InetAddress.getByName(hostIP);
-			new SendThread(hostAddress, portNumber).start();  //Start SendThread
-			new ReceiveThread(portNumber).start();	//Start ReceiveThread
-			new IntermediateThread(hostAddress, portNumber).start(); //Start IntermediateThread
+			int hostPort = Integer.parseInt(args[1]);//Take port number from command line
+			new SendThread(receiverAddress, receiverPort).start();  //Start SendThread
+			new ReceiveThread(hostPort).start();	//Start ReceiveThread
+			new IntermediateThread(hostAddress, hostPort).start(); //Start IntermediateThread
 		}catch(UnknownHostException e){	//Just throw some generic error if problem
 			System.out.println("Error: Unknown Host");
 			System.exit(1);
@@ -42,34 +48,30 @@ public class Peer{
 
 	//This is the thread that sends the messages to all other peers
 	static class SendThread extends Thread{
-		private InetAddress hostAddress;
-		private int portNumber;
-		//Receive and store hostAddress and portNumber
-		SendThread(InetAddress newHostAddress, int newPortNumber){
-			hostAddress = newHostAddress;
-			portNumber = newPortNumber;
+		private InetAddress receiverAddress;
+		private int receiverPort;
+		//Receive and store receiverAddress and receiverPort
+		SendThread(InetAddress newReceiverAddress, int newReceiverPort){
+			receiverAddress = newReceiverAddress;
+			receiverPort = newReceiverPort;
 		}
 
 		public void run(){
 			byte[] buffer = new byte[256];
-			byte[] ender = new byte[1];
 			DatagramPacket packet;
 			String currentMessage;
 			System.out.println("This is the thread that sends stuff");
 			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
             try{
-    			currentMessage = stdIn.readLine();	//Take user input from system
-    			buffer = currentMessage.getBytes();
-    			ender[0] = (byte) '\n';
-    			DatagramSocket dataSocket = new DatagramSocket(portNumber);
-    			//THING TO CHANGE: This function is going to have to use a linkedlist of addresses
-    			for(int i = 0; i < buffer.length; i++){
-    				packet = new DatagramPacket(buffer, i, 1, hostAddress, portNumber);
-    				dataSocket.send(packet);
-    			}
+				while(true){
+					currentMessage = stdIn.readLine();	//Take user input from system
+					buffer = currentMessage.getBytes();
+					DatagramSocket dataSocket = new DatagramSocket();
+					//THING TO CHANGE: This function is going to have to use a linkedlist of addresses
+					packet = new DatagramPacket(buffer, 0, buffer.length, receiverAddress, receiverPort);
+					dataSocket.send(packet);
+				}
 
-    			packet = new DatagramPacket(ender, 0, 1, hostAddress, portNumber);
-    			dataSocket.send(packet);
             }catch(IOException e){ //If error, tell user
                 System.out.println("Error:" + e);
             }
@@ -111,7 +113,7 @@ public class Peer{
 					packet = new DatagramPacket(buffer, buffer.length);
 					dataSocket.receive(packet);
 					newCharacter = new String(packet.getData(), 0, packet.getLength());
-					System.out.print(newCharacter);
+					System.out.println(newCharacter);
 				}
 
 			}catch(IOException e){	//If error, tell user
