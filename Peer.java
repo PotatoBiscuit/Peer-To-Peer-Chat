a42f1f3ecf48a2d288fbb77e03876dbfd703c4ff
Erik <eld66@nau.edu>
2017-02-12 18:19:16 -0700

    Added support for sending and receiving protocol 1 and 5 messages

--- Peer.java
+++ Peer.java
@@ -15,6 +15,7 @@ public class Peer{
 	public static LinkedList<PeerInfo> peerList = new LinkedList<PeerInfo>();
     public static boolean stillConnected = true;
 	public static Scanner userInput = new Scanner(System.in);
+	public static String name;
 
 	public static void main(String[] args){
 		//IP and port number received when running program
@@ -24,16 +25,14 @@ public class Peer{
             System.exit(1);
         }
 		String hostIP = args[0];	//Take user IP from command line
+		
+		System.out.print("What is your name? ");
+		name = userInput.nextLine();
+		
 		try{
-			//HostIP converted into address format
-			System.out.print("Host IP you would like to send messages to: ");
-			InetAddress receiverAddress = InetAddress.getByName(userInput.nextLine());
-			System.out.print("Port number to send to: ");
-			int receiverPort = userInput.nextInt();
-			
 			InetAddress hostAddress = InetAddress.getByName(hostIP);
 			int hostPort = Integer.parseInt(args[1]);//Take port number from command line
-			new SendThread(receiverAddress, receiverPort).start();  //Start SendThread
+			new SendThread(hostAddress, hostPort).start();  //Start SendThread
 			new ReceiveThread(hostPort).start();	//Start ReceiveThread
 			new IntermediateThread(hostAddress, hostPort).start(); //Start IntermediateThread
 		}catch(UnknownHostException e){	//Just throw some generic error if problem
@@ -50,32 +49,63 @@ public class Peer{
 	static class SendThread extends Thread{
 		private InetAddress receiverAddress;
 		private int receiverPort;
-		//Receive and store receiverAddress and receiverPort
-		SendThread(InetAddress newReceiverAddress, int newReceiverPort){
-			receiverAddress = newReceiverAddress;
-			receiverPort = newReceiverPort;
+		private InetAddress senderAddress;
+		private int senderPort;
+		private BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+		DatagramSocket dataSocket;
+		private DatagramPacket packet;
+		private byte[] buffer;
+		
+		public SendThread(InetAddress newSenderAddress, int newSenderPort){
+			senderAddress = newSenderAddress;
+			senderPort = newSenderPort;
+			try{
+				dataSocket = new DatagramSocket();
+			}catch(IOException e){
+				System.out.println("Error:" + e);
+			}
 		}
 
 		public void run(){
-			byte[] buffer = new byte[256];
-			DatagramPacket packet;
-			String currentMessage;
-			System.out.println("This is the thread that sends stuff");
-			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-            try{
+			try{
+				joinNetwork();
 				while(true){
-					currentMessage = stdIn.readLine();	//Take user input from system
-					buffer = currentMessage.getBytes();
-					DatagramSocket dataSocket = new DatagramSocket();
-					//THING TO CHANGE: This function is going to have to use a linkedlist of addresses
-					packet = new DatagramPacket(buffer, 0, buffer.length, receiverAddress, receiverPort);
-					dataSocket.send(packet);
+					sendMessage(stdIn.readLine());
 				}
-
-            }catch(IOException e){ //If error, tell user
-                System.out.println("Error:" + e);
-            }
-
+			}catch(IOException e){
+				System.out.println("Error:" + e);
+			}
+		}
+		
+		public void joinNetwork() throws IOException{ //Send protocol 1 message
+			String decision;
+			do{
+				System.out.print("Would you like to connect to a peer? (Y/N) ");
+			}while(!(decision = userInput.nextLine()).equals("Y") && !decision.equals("N"));
+			if(decision.equals("Y")){
+				System.out.print("Please give us the IP Address: ");
+				receiverAddress = InetAddress.getByName(userInput.nextLine());
+				System.out.print("Port number to send to: ");
+				receiverPort = userInput.nextInt();
+				
+				peerList.add(new PeerInfo(receiverAddress, receiverPort));
+				
+				String message = "1:" + senderPort + ":" + senderAddress.toString();
+				buffer = new byte[256];
+				buffer = message.getBytes();
+				packet = new DatagramPacket(buffer, 0, buffer.length, receiverAddress, receiverPort);
+				dataSocket.send(packet);
+			}
+		}
+		
+		public void sendMessage(String message) throws IOException{	//Send protocol 5 message
+			message = "5:" + name + ":" + message;
+			buffer = new byte[256];
+			buffer = message.getBytes();
+			for(PeerInfo peer : peerList){
+				packet = new DatagramPacket(buffer, 0, buffer.length, peer.hostIP, peer.portNum);
+				dataSocket.send(packet);
+			}
 		}
 	}
 
@@ -88,37 +118,67 @@ public class Peer{
 			portNumber = newPortNumber;
 		}
 		public void run(){
-			System.out.println("This is a helper thread");
+			;
 		}
 	}
 
 	//This is the thread that receives messages sent
 	static class ReceiveThread extends Thread{
 		private int portNumber;
+		private DatagramSocket receiveSocket;
+		private DatagramSocket responseSocket;
+		private DatagramPacket packet;
+		private byte[] buffer; //Create buffer to hold messages
 
 		//Receive and store portNumber
 		ReceiveThread(int newPortNumber){
 			portNumber = newPortNumber;
+			try{
+				receiveSocket = new DatagramSocket(portNumber);
+				responseSocket = new DatagramSocket();
+			}catch(IOException e){
+				System.out.println("Error:" + e);
+			}
 		}
 
 		public void run(){
-			System.out.println("This is the thread that receives stuff");
 			String newCharacter;
-			DatagramPacket packet;
-			byte[] buffer = new byte[256]; //Create buffer to hold messages
+			String[] partsOfMessage;
 			try{
 				//Create listening port
-				DatagramSocket dataSocket = new DatagramSocket(portNumber);
 				while(stillConnected){	//Listen for messages while still connected
+					buffer = new byte[256];
 					packet = new DatagramPacket(buffer, buffer.length);
-					dataSocket.receive(packet);
+					receiveSocket.receive(packet);
 					newCharacter = new String(packet.getData(), 0, packet.getLength());
-					System.out.println(newCharacter);
+					newCharacter = newCharacter.replace("/", "");
+					partsOfMessage = newCharacter.split(":");
+					if(partsOfMessage[0].equals("1")){
+						broadcastJoin(partsOfMessage[2], partsOfMessage[1]);
+					}
+					if(partsOfMessage[0].equals("5")){
+						displayMessage(partsOfMessage[1], partsOfMessage[2]);
+					}
 				}
 
 			}catch(IOException e){	//If error, tell user
 				System.out.println("Error:" + e);
 			}
+		}
+		
+		public void broadcastJoin(String hostIP, String portNum) throws IOException{ //Handle protocol 1 message
+			String message = "2:" + portNum + ":" + hostIP;
+			buffer = new byte[256];
+			buffer = message.getBytes();
+			for(PeerInfo peer : peerList){
+				packet = new DatagramPacket(buffer, 0, buffer.length, peer.hostIP, peer.portNum);
+				responseSocket.send(packet);
+			}
+			peerList.add(new PeerInfo(InetAddress.getByName(hostIP), Integer.parseInt(portNum)));
+		}
+		
+		public void displayMessage(String senderName, String senderMessage){ //Handle protocol 5 message
+			System.out.println(senderName + ":" + senderMessage);
 		}
 	}
 }
--- PeerInfo.java
+++ PeerInfo.java
@@ -2,4 +2,9 @@ import java.net.InetAddress;
 public class PeerInfo{
 	public InetAddress hostIP;
 	public int portNum;
+	
+	public PeerInfo(InetAddress newIP, int newPort){
+		hostIP = newIP;
+		portNum = newPort;
+	}
 }
